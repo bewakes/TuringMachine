35a7075b0d4b9dafeb02343bb210e8bc6956a307
Bibek Pandey <bibek@janakitech.com>
2016-12-07 18:17:48 +0545

    added module to format cells

---
+++ Formatting.hs
@@ -0,0 +1,26 @@
+import System.Process
+
+data Box = Box (String, String, String)
+data Boxes = Boxes [Box]
+
+instance Show (Box) where
+    show (Box (top, middle, bottom)) = top ++ "\n" ++ middle ++ "\n" ++ bottom
+
+instance Show (Boxes) where
+    show (Boxes []) = ""
+    show (Boxes (b:[])) = show b
+    show (Boxes (Box (t, m, b): boxes)) = top ++ "\n" ++ middle ++ "\n" ++ bottom
+        where middle = m ++ foldl (++) "" $ excludingFirstOfMiddles boxes
+              top = replicate (length middle) '-'
+              bottom = top
+              cat = foldl (++) ""
+              excludingFirstOfMiddles lst = (map (\(Box(t,m,b)) -> tail m) lst)
+
+
+getBox :: String -> Box
+getBox value = Box (t, m, b)
+    where m = "| " ++ value ++ " |"
+          t = replicate  (length m) '-'
+          b = t
+
+main = putStrLn $ show $ Boxes [getBox "b"]
