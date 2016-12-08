b848f02a528b126b880f9d68ba1a1701afab9ef7
Bibekpandey <spirit_bibek@yahoo.com>
2016-12-08 22:48:05 +0000

    added some data

--- Machine.hs
+++ Machine.hs
@@ -1,8 +1,9 @@
 import Tape
+import Data.Map
+import System.Process
+import Control.Concurrent
 
-data State = State String deriving(Show)
-
-
+data State = State String deriving(Show, Ord, Eq)
 
 data Machine = Machine 
     {
@@ -12,24 +13,46 @@ data Machine = Machine
         function        :: [((State, String), (State, String))],
         alphabet        :: [String], -- consists of blank and left End symbol(>)
         tape            :: Tape,
-        headPosition    :: Int
+        headPosition    :: Int,
+        currentState    :: State
     } deriving(Show)
 
+halt = State "h"
+q0 = State "q0"
+q1 = State "q1"
 
-c = getCell "b"
-t = Tape []
+_BLANK = ""
+_LEFT_END = ">"
 
-state = State "halt"
+alpha = ["0","1", _BLANK, _LEFT_END]
+
+_states = [halt, q0, q1]
+
+funclist = [
+        ((q0, ""), (halt, "")),
+        ((q0, "0"), (q1, "1")),
+        ((q0, "1"), (q1, "0")),
+        ((q0, ">"), (q0, "->")),
+        ((q1, ""), (halt, "")),
+        ((q1, "1"), (q0, "->")),
+        ((q1, "0"), (q0, "->"))
+    ]
+
+step funclist state = Data.Map.lookup state hmap
+    where hmap = fromList funclist
+
+t = Tape (Prelude.map (Cell . show) [1,0,1,1,0,1])
+
 
 m = Machine { 
-    states = [state],
-    initialState = state,
-    haltingStates = [state],
-    function = [],
+    states = _states,
+    initialState = q0,
+    haltingStates = [halt],
+    function = funclist,
     tape = t,
     headPosition = 4,
     alphabet = []
 }
 
 
-main = putStrLn $ show m
+main = putStrLn $ show $ step funclist (q0, "0") 
