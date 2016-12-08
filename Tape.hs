0189c4df41a33754e4e69125a3da9207d8268001
Bibek Pandey <bibek@janakitech.com>
2016-12-08 17:11:25 +0545

    renamed data

--- Tape.hs
+++ Tape.hs
@@ -1,25 +1,25 @@
 import System.Process
 import Control.Concurrent
 
-data Box = Box (String, String, String)
-data Boxes = Boxes [Box]
+data Cell = Cell (String, String, String)
+data Tape = Tape [Cell]
 
-instance Show (Box) where
-    show (Box (top, middle, bottom)) = top ++ "\n" ++ middle ++ "\n" ++ bottom
+instance Show (Cell) where
+    show (Cell (top, middle, bottom)) = top ++ "\n" ++ middle ++ "\n" ++ bottom
 
-instance Show (Boxes) where
-    show (Boxes []) = ""
-    show (Boxes (b:[])) = show b
-    show (Boxes (Box (t, m, b): boxes)) = top ++ "\n" ++ middle ++ "\n" ++ bottom
+instance Show (Tape) where
+    show (Tape []) = ""
+    show (Tape (b:[])) = show b
+    show (Tape (Cell (t, m, b): boxes)) = top ++ "\n" ++ middle ++ "\n" ++ bottom
         where middle = m ++ ( cat $ excludingFirstOfMiddles boxes)
               top = replicate (length middle) '-'
               bottom = top
               cat = foldl (++) ""
-              excludingFirstOfMiddles lst = (map (\(Box(t,m,b)) -> tail m) lst)
+              excludingFirstOfMiddles lst = (map (\(Cell(t,m,b)) -> tail m) lst)
 
 
-getBox :: String -> Box
-getBox value = Box (t, m, b)
+getCell :: String -> Cell
+getCell value = Cell (t, m, b)
     where m = "| " ++ value ++ " |"
           t = replicate  (length m) '-'
           b = t
@@ -33,6 +33,6 @@ display = disp 10
                 putStrLn $ str
                 threadDelay 800000
                 disp (n-1)
-            where str = show $ Boxes $ map getBox (map show [1..n])
+            where str = show $ Tape $ map getCell (map show [1..n])
 main = do
     display
